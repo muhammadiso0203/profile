ce49cc92cf150f598275386fe8693132ba67bd3f
muhammadiso0203 <muhammadiso0203@gmail.com>
2025-03-23 12:00:48 +0500

    first commit

---
+++ src/entities/user.js
@@ -0,0 +1,10 @@
+export class User {
+	constructor(firstName, lastName, email, password, phoneNumber, address) {
+		this.first_name = firstName;
+		this.last_name = lastName;
+		this.email = email;
+		this.password = password;
+		this.phone_number = phoneNumber;
+		this.address = address;
+	}
+}
